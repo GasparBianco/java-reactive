d8ab72c09ff39ce8432d1715e7b45f6878b8f148
Gaspar <gasparbianco98@gmail.com>
2025-06-21 00:26:49 -0300

    hot and cold publisher

---
+++ src/main/java/org/reactivo/clase05hotAndColdPublishers/Leccion01ColdPublisher.java
@@ -0,0 +1,32 @@
+package org.reactivo.clase05hotAndColdPublishers;
+
+import org.reactivo.common.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Leccion01ColdPublisher {
+    private static final Logger log = LoggerFactory.getLogger(Leccion01ColdPublisher.class);
+
+    public static void main(String[] args) {
+
+        var flux = Flux.create(fluxSink -> {
+            log.info("invocado");
+            for (int i = 0; i < 30; i++) {
+                fluxSink.next(i);
+            }
+            fluxSink.complete();
+        }).delayElements(Duration.ofMillis(500))
+                .share();
+
+
+        flux.subscribe(Utils.subscriber("sub1"));
+        Utils.awaitSeconds(2);
+        flux.subscribe(Utils.subscriber("sub2"));
+
+        Utils.awaitSeconds(10);
+
+    }
+}
---
+++ src/main/java/org/reactivo/clase05hotAndColdPublishers/Leccion02HotPublisher.java
@@ -0,0 +1,52 @@
+package org.reactivo.clase05hotAndColdPublishers;
+
+import org.reactivo.common.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Leccion02HotPublisher {
+    private static final Logger log = LoggerFactory.getLogger(Leccion02HotPublisher.class);
+
+    public static void main(String[] args) {
+
+        var movieFlux = movieStream().share();
+
+        Utils.awaitSeconds(2);
+
+        movieFlux
+                .take(4)
+                .subscribe(Utils.subscriber("sam"));
+
+        Utils.awaitSeconds(3);
+
+        movieFlux
+                .take(3)
+                .subscribe(Utils.subscriber("mike"));
+
+
+        Utils.awaitSeconds(15);
+
+    }
+
+    private static Flux<String> movieStream() {
+        return Flux.generate(
+                        () -> {
+                            log.info("received the request");
+                            return 1;
+                        },
+                        (state, sink) -> {
+                            var scene = "movie scene " + state;
+                            log.info("playing {}", scene);
+                            sink.next(scene);
+                            return ++state;
+                        }
+                )
+                .take(10)
+                .delayElements(Duration.ofSeconds(1))
+                .cast(String.class);
+    }
+
+}
